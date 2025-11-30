294c5711758069d7dd937ceb3a631a83dcd0690d
Ramindu2001 <ramindurantharu@gmail.com>
2025-11-30 17:16:29 +0530

    update Laptop entity

--- src/main/java/com/example/OneToOne/entity/Laptop.java
+++ src/main/java/com/example/OneToOne/entity/Laptop.java
@@ -1,9 +1,6 @@
 package com.example.OneToOne.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,4 +16,7 @@ public class Laptop {
     private String brand;
     private String model;
     private int ram;
+
+    @OneToOne(mappedBy = "laptop")
+    private Student student;
 }
