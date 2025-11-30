958db029a56018f345f67e9a86f5b15a58ddb508
Ramindu2001 <ramindurantharu@gmail.com>
2025-11-30 17:09:37 +0530

    create Laptop entity

---
+++ src/main/java/com/example/OneToOne/entity/Laptop.java
@@ -0,0 +1,22 @@
+package com.example.OneToOne.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Laptop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String brand;
+    private String model;
+    private int ram;
+}
